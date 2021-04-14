a330b4b7f694bb3061da353abcf9279536dd669f
haggishunt56 <whatevermaanhotmail.com>
2021-04-14 11:47:48 +0100

    Amended React code to remove reference to @not-govuk/anchor component. This component was causing an error as the module could not be found even after installing it a number of times. I believe this was also causing automated checks to fail. Pushing to see if automated checks now pass.

--- components/pagination/src/Pagination.tsx
+++ components/pagination/src/Pagination.tsx
@@ -1,6 +1,5 @@
 import React, { FC, createElement as h } from 'react';
 import { StandardProps, classBuilder } from '@not-govuk/component-helpers';
-import { A } from '@not-govuk/anchor';
 
 import '../assets/Pagination.scss';
 
@@ -42,29 +41,29 @@ export const Pagination: FC<PaginationProps> = ({
       <div className={classes('summary')}>Showing {resultsFrom} - {resultsTo} of {results} results</div>
       <ul className={classes('list-items')}>
         <li className={classes('item')} id="prevButton">
-          <A className={classes('link')} href="#2">
+          <a className={classes('link')} href="#2">
             <span aria-hidden="true" role="presentation">&laquo;</span> Previous
-          </A>
+          </a>
         </li>
         <li className={classes('item')}>
-          {page>2 ? <A className={classes('link')} href="#1">{page-2}</A> : null}
+          {page>2 ? <a className={classes('link')} href="#1">{page-2}</a> : null}
         </li>
         <li className={classes('item')}>
-          {page>1 ? <A className={classes('link')} href="#2">{page-1}</A> : null}
+          {page>1 ? <a className={classes('link')} href="#2">{page-1}</a> : null}
         </li>
         <li className={classes('item')}>
-          <A className={classes('link-current')} href="#3" aria-current="true">{page}</A>
+          <a className={classes('link-current')} href="#3" aria-current="true">{page}</a>
         </li>
         <li className={classes('item')}>
-          {resultsTo==results ? null : <A className={classes('link')} href="#4">{page-(-1)}</A>}
+          {resultsTo==results ? null : <a className={classes('link')} href="#4">{page-(-1)}</a>}
         </li>
         <li className={classes('item')}>
-          {resultsTo <= results - resultsPerPage ? <A className={classes('link')} href="#5">{page-(-2)}</A> : null}
+          {resultsTo <= results - resultsPerPage ? <a className={classes('link')} href="#5">{page-(-2)}</a> : null}
         </li>
         <li className={classes('item')} id="nextButton">
-          <A className={classes('link')} href="#5">
+          <a className={classes('link')} href="#5">
             Next <span aria-hidden="true" role="presentation">&raquo;</span>
-          </A>
+          </a>
         </li>
       </ul>
     </div>
